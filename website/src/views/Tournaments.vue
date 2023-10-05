71f63ec8ffc3e8993b7746d6d09d9f92c2a1f0c5
collinshayden <collinshayden171@gmail.com>
2023-10-05 18:41:26 -0400

    added prize fund to tournament desc

--- website/src/views/Tournaments.vue
+++ website/src/views/Tournaments.vue
@@ -1,31 +1,29 @@
-import tournament
-
 <template>
   <h2 class="title">Upcoming Tournaments</h2>
   <div>
     <Tournament
       title="1st UVM Fall Open - October 14/15"
-      format="4SS G80+10, Open/U1400, EF $45/$30"
+      format="4SS G80+10, Open/U1400, EF $45/$30, Prizes $500"
       link="https://new.uschess.org/1st-uvm-fall-open-national-chess-day"
     />
     <Tournament
       title="1st Nashua Tornado - October 21"
-      format="4SS G55d5, Premier (1400+)/U1875/U1275, EF $55/$40/$30"
+      format="4SS G55d5, Premier (1400+)/U1875/U1275, EF $55/$40/$30, Prizes $2700"
       link="https://new.uschess.org/1st-nashua-tornado-0"
     />
     <Tournament
       title="Bennington Fall Open - October 28"
       format="4SS G60+5, Open/U1750/U1350, EF $25"
-      link="https://new.uschess.org/bennington-fall-open-2023"
+      link="https://new.uschess.org/bennington-fall-open-2023, Prizes $750"
     />
     <Tournament
       title="Vermont Open - November 18/19"
-      format="4SS G60+30, Championship (1700+)/U1825/U1425, EF $50"
+      format="4SS G60+30, Championship (1700+)/U1825/U1425, EF $50, Prizes $2000"
       link="https://new.uschess.org/2023-vermont-open"
     />
     <Tournament
       title="1st RAR Open - December 16"
-      format="4SS G55d5, Premier (1600+)/U2025/U1225, EF $50/$35/$30"
+      format="4SS G55d5, Premier (1600+)/U2025/U1225, EF $50/$35/$30, Prizes $2500"
       link="https://new.uschess.org/1st-rar-open"
     />
   </div>
