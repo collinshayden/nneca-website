a074405854cdf3ebb5367f679106297138cb6851
collinshayden <collinshayden171@gmail.com>
2023-12-18 01:23:36 -0500

    removed GP tournaments, will just use tournaments page for that

--- website/src/views/GrandPrix.vue
+++ website/src/views/GrandPrix.vue
@@ -26,33 +26,6 @@
         >North-West Chess Grand Prix rules</a
       >.
     </p>
-    <h3>2024 Grand Prix Events (more to be added)</h3>
-    <div class="events">
-      <Tournament
-        title="New England Grand Prix Inaugural Event - January 13"
-        format="4SS G55d5, Premier (1600+), U2125, U1825, U1525, U1225, EF $50/$40/$35/$30/$25, Prizes $2700"
-        link="https://onlineregistration.cc/tournaments/index.php?view=zNTizdLa&tid=m6CioQ=="
-        link_desc="Details here"
-      />
-      <Tournament
-        title="3rd UVM Spring Open - February 3/4"
-        format="4SS G90+30, Open/U1400, EF $50/$35, Prizes $500"
-        link="https://new.uschess.org/3rd-uvm-spring-open"
-        link_desc="Details here"
-      />
-      <Tournament
-        title="Nashua Grand Prix Tornado - February 24"
-        format="4SS G55d5, Premier (1600+), U2025, U1625, U1225, EF $50/$40/$35/$30, Prizes $2400"
-        link="https://onlineregistration.cc/tournaments/index.php?view=zNTizdLa&tid=m6Ciog=="
-        link_desc="Details here"
-      />
-      <Tournament
-        title="1st New England Amateur Team Championship - March 23/24"
-        format="4SS G90+30, U1800 (avg), EF $200/team, Prizes $2000"
-        link="https://newenglandchess.org/team"
-        link_desc="Details here"
-      />
-    </div>
 
     <h3>Rules</h3>
     <ol type="1">
