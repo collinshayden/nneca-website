5b2e17ceca1a8f6bce948c3203ea4f983761b264
collinshayden <collinshayden171@gmail.com>
2024-01-25 20:02:30 -0500

    table now only displays total points (better for mobile)

--- website/src/views/Standings.vue
+++ website/src/views/Standings.vue
@@ -8,10 +8,10 @@
         href="https://docs.google.com/spreadsheets/d/1sUah6u_FEgw35qm7nhImlAmHUlXG5V3zD0vq2hqaI00/edit?usp=sharing"
         target="_blank"
         >this spreadsheet</a
-      >, which includes links to players' USCF profiles and the ability to sort
-      by different columns. There is also another tab on that spreadsheet that
-      details the current prize fund. If you believe there to be an error in the
-      standings, please reach out to Joseph Truelson.
+      >, which includes the breakdown of points by event, links to players' USCF
+      profiles, the ability to sort by different columns, and details of the
+      current prize fund. If you believe there to be an error in the standings,
+      please reach out to Joseph Truelson.
     </p>
   </div>
   <div class="table-container">
@@ -26,7 +26,6 @@
         <td>{{ row[2] }}</td>
         <td>{{ row[3] }}</td>
         <td>{{ row[4] }}</td>
-        <td>{{ row[5] }}</td>
       </tr>
     </table>
   </div>
@@ -43,17 +42,17 @@ export default {
   },
   async created() {
     const response = await axios.get(
-      "https://sheets.googleapis.com/v4/spreadsheets/1sUah6u_FEgw35qm7nhImlAmHUlXG5V3zD0vq2hqaI00/values/Standings!A1:F40",
+      "https://sheets.googleapis.com/v4/spreadsheets/1sUah6u_FEgw35qm7nhImlAmHUlXG5V3zD0vq2hqaI00/values/Standings!A1:E40",
       {
         params: {
-          // this is an API key that gives read access to Google Sheets owned by a Google account specifically made for this page. 
-          // while not the best practice to put an API key in a public GitHub repo, there are no security issues as this is the 
-          // only spreadsheet that will ever live on this account. 
+          // this is an API key that gives read access to Google Sheets owned by a Google account specifically made for this page.
+          // while not the best practice to put an API key in a public GitHub repo, there are no security issues as this is the
+          // only spreadsheet that will ever live on this account.
           key: "AIzaSyAw5QCd93cJMneomg2V8dW87RFB-UXsaiI",
         },
       }
     );
-    console.log("Response data:", response.data); 
+    console.log("Response data:", response.data);
     this.rows = response.data.values;
   },
 };
@@ -61,21 +60,21 @@ export default {
 
 <style scoped>
 .table-container {
-  width: 100%; 
+  width: 100%;
 }
 
 .table-container table {
-  width: 60%; 
+  width: 60%;
   border-collapse: collapse;
   margin-left: auto;
   margin-right: auto;
 }
 
 .table-container td {
-  text-align: center; 
-  vertical-align: middle; 
-  border: 1px solid black; 
-  padding: 10px; 
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid black;
+  padding: 10px;
 }
 
 .bold-row {
@@ -93,11 +92,20 @@ p {
 }
 
 @media (max-width: 600px) {
-p {
-  width: 75%
-}
- .table-container td {
+  p {
+    width: 75%;
+  }
+  .table-container td {
     padding: 5px;
- }
+  }
+}
+
+@media (max-width: 400px) {
+  p {
+    width: 80%;
+  }
+  .table-container td {
+    padding: 2px;
+  }
 }
 </style>
