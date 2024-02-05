2da4c858d3177c42cdfda7d332cd7f507a15aec2
collinshayden <collinshayden171@gmail.com>
2024-02-05 16:21:05 -0500

    added BSO

--- website/src/views/Tournaments.vue
+++ website/src/views/Tournaments.vue
@@ -2,12 +2,6 @@
   <h2 class="title">Upcoming Grand Prix Events</h2>
   <div>
     <Tournament
-      title="3rd UVM Spring Open - February 3/4"
-      format="4SS G90+30, Open/U1400, EF $50/$35, Prizes $500"
-      link="https://new.uschess.org/3rd-uvm-spring-open"
-      link_desc="Details here"
-    />
-    <Tournament
       title="Nashua Grand Prix Tornado - February 17"
       format="4SS G55d5, Premier (1600+), U2025, U1625, U1225, EF $50/$40/$35/$30, Prizes $2400"
       link="https://onlineregistration.cc/tournaments/index.php?view=zNTizdLa&tid=m6Ciog=="
@@ -35,6 +29,12 @@
       title="1st New England Amateur Team Championship - April 13/14"
       format="4SS G90+30, U1800 (avg), EF $200/team, Prizes $2000"
       link="https://newenglandchess.org/team"
+      link_desc="Details here"
+    />
+    <Tournament
+      title="2024 Bennington Spring Open - May 11"
+      format="4SS G55+5, Open/U1750/U1350, EF $30, Prizes $670"
+      link="https://new.uschess.org/bennington-spring-open-2024"
       link_desc="Details here"
     />
   </div>
