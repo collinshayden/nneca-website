7ee470494c1c22abb0950c145beb28137a51f96f
collinshayden <collinshayden171@gmail.com>
2024-06-03 14:17:38 -0400

    added chess festival + put multipliers on event titles

--- website/src/views/Tournaments.vue
+++ website/src/views/Tournaments.vue
@@ -2,25 +2,37 @@
   <h2 class="title">Upcoming Grand Prix Events</h2>
   <div>
     <Tournament
-      title="2024 New England Class Championship - June 15/16"
+      title="2024 New England Class Championship - 4x Multiplier - June 15/16"
       format="5SS 3 G60+30 + 2 G90+30, Master-Expert/Class A/B/C/D/E, EF $120/$80 for A-D/$60 for E, Prizes $6500"
       link="https://new.uschess.org/2024-new-england-class-championship"
       link_desc="Details here"
     />
     <Tournament
-      title="1st Concord Open - June 29"
+      title="1st Concord Open - 3x Multiplier - June 29"
       format="4SS G55d5, Premier (1700+)/U1925/U1325, EF $55/$40/$30, Prizes $2000"
       link="https://new.uschess.org/1st-concord-open"
       link_desc="Details here"
     />
-    <Tournament 
-      title="1st Autumn Classic - September 28/29"
+    <Tournament
+      title="Northern New England G55 Championship - 2x Multiplier - August 8"
+      format="4SS G55d5, Open, EF FREE to Festival players, $30 otherwise, Prizes $500"
+      link="https://new.uschess.org/northern-new-england-chess-festival"
+      link_desc="Details here"
+    />
+    <Tournament
+      title="Northern New England Chess Festival - 4x Multiplier - August 9/10/11"
+      format="6SS G90+30, Premier (1800+)/U2025/U1425, EF $150/$100/$80, Prizes $6000"
+      link="https://new.uschess.org/northern-new-england-chess-festival"
+      link_desc="Details here"
+    />
+    <Tournament
+      title="1st Autumn Classic - 2x Multiplier - September 28/29"
       format="4SS G90+30, Open/U1700, EF $45/$30, Prizes $500"
       link="https://new.uschess.org/1st-autumn-classic"
       link_desc="Details here"
     />
     <Tournament
-      title="1st Origami Open - October 25-27"
+      title="1st Origami Open - 4x Multiplier - October 25-27"
       format="6SS 4 G60+30, 2 G90+30, 1700+/U1925/U1325, EF $125/90/65, Prizes $5500"
       link="https://new.uschess.org/1st-origami-open"
       link_desc="Details here"
