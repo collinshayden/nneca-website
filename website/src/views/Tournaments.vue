5f178dbecc9ad6bb391cc65f668715b090a66e96
collinshayden <collinshayden171@gmail.com>
2024-07-21 10:58:21 -0400

    added BFO

--- website/src/views/Tournaments.vue
+++ website/src/views/Tournaments.vue
@@ -36,6 +36,12 @@
       link_desc="Details here"
     />
     <Tournament
+      title="Bennington Fall Open - 2x Multiplier - October 5"
+      format="4SS G60+5, Open/U1750/U1350, EF $30, Prizes $780"
+      link="https://new.uschess.org/bennington-fall-open"
+      link_desc="Details here"
+    />
+    <Tournament
       title="1st Origami Open - 4x Multiplier - October 25-27"
       format="6SS 4 G60+30, 2 G90+30, 1700+/U1925/U1325, EF $125/90/65, Prizes $5500"
       link="https://new.uschess.org/1st-origami-open"
